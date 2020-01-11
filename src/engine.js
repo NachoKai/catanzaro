e6c2421dc41f80ddf8e2eef8604a90208cd07980
NachoKai <47578311+NachoKai@users.noreply.github.com>
2020-01-10 22:36:17 -0300

    .

--- src/engine.js
+++ src/engine.js
@@ -61,7 +61,7 @@ class Engine {
     //renderiza el mapa
     async renderMap() {
         // descargar el archivo del mapa city.json
-        const response = await fetch("https://raw.githubusercontent.com/NachoKai/catanzaro/gh-pages/maps/city.json");
+        const response = await fetch("/maps/city.json");
         const result = await response.json();
         for (let y = 0; y <= this.mapSize.y - 1; y++) {
             for (let x = 0; x <= this.mapSize.x - 1; x++) {
@@ -76,7 +76,20 @@ class Engine {
     }
 
     /* todos los assets que queramos cargar en el juego, como árboles, carteles, piedras, etc */
-    renderEnvironment() {        
+    renderEnvironment() {
+        // const response = await fetch("/maps/city.json");
+        // const result = await response.json();
+        // for (let y = 0; y <= this.mapSize.y - 1; y++) {
+        //     for (let x = 0; x <= this.mapSize.x - 1; x++) {
+        //         const tile = result[y][x];
+        //         this.ctx.background.drawImage(
+        //             this.images[tile.background],
+        //             x * this.sizeTile,
+        //             y * this.sizeTile
+        //         );
+        //     }
+        // }
+
         this.ctx.foreground.drawImage(this.images.tree, 25, 100);
         this.ctx.foreground.drawImage(this.images.poster, 150, 40);
         this.ctx.foreground.font = "9pt Helvetica";
